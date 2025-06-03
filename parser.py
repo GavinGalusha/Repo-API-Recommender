286369310a81bfe7cf6fce10b5901e40cd5d800d
andrewhamara <94264936+andrewhamara@users.noreply.github.com>
2025-06-03 10:44:01 +0300

    crud regex

--- parser.py
+++ parser.py
@@ -3,19 +3,18 @@ from springboot_regex import get_endpoint_regex
 
 
 def print_files_in_dir_nested(directory):
-    for filename in os.listdir(directory):
-        if filename.endswith(".java"):
-            with open(os.path.join(directory, filename)) as f:
-                text = f.read().strip()
-                c, r, u, d = get_endpoint_regex(text)
-                for x in [c,r,u,d]:
-                    if x:
-                        print(x)
-            continue
-        elif os.path.isdir(filename):
-            print_files_in_dir_nested(filename)
-        else:
-            continue
+    for root, _, files in os.walk(directory):
+        for filename in files:
+            if filename.endswith(".java"):
+                with open(os.path.join(root, filename)) as f:
+                    text = f.read().strip()
+                    c, r, u, d = get_endpoint_regex(text)
+                    for x in [c,r,u,d]:
+                        if x:
+                            print(x)
+                continue
+            else:
+                continue
 
 
 cwd = os.getcwd()
