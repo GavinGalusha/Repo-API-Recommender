bcc648f998ae2718996863c130001ead1a4687fd
Gavin_Galusha <gavingalusha@gmail.com>
2025-06-11 10:56:52 +0300

    refined prompt

--- src/llm.py
+++ src/llm.py
@@ -1,31 +1,42 @@
+import os
+import json
 import lmstudio as lms
 import chromadb
 
+
 # Configuration
 SERVER_API_HOST = "localhost:1234"
-
 # Connect to the LLM server
-chroma_client = chromadb.PersistentClient(path="database.chroma")
+
+chroma_client = chromadb.PersistentClient(path="Database/database.chroma")
 collection = chroma_client.get_or_create_collection(name="my_collection")
-def find_similar_apis(text_input, top_k=4):
+def find_similar_apis(text_input, top_k = 4):
     results = collection.query(
-        query_texts=[text_input],
-        n_results=4
+    query_texts=[input],
+    n_results=4
     )
-    return results[:top_k]
+    return results
+
+
 
 lms.configure_default_client(SERVER_API_HOST)
 model = lms.llm("ibm/granite-3.1-8b")
 
-prompt = """
+prompt = """ 
 
-Parse the below text to only include the descriptions from documents. List each description from documents whether it is 'Empty' or a description of an api"""
+is the input api. You are an api reccomender. Take in the descriptions of an API or multiple APIs, and use the information below to reccomend new API's that would complement the input API, using the api's below as in"""
 
-user_input = input("Input an api description to receive similar api's that could be useful: \n")
+input = input("Input an api description to receive similar api's that could be useful: \n")
 
-response = find_similar_apis(user_input)
 
-result = model.respond(prompt + str(response.items()))
+response = find_similar_apis(input)
+
+
+print("Rag Response", response)
+print()
+result = model.respond(input + prompt + str(response.items()))
 print("final structured output:")
 print("Your input: ", input)
 print(result)
+
+
