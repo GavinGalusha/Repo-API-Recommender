a7ac6962a2d222c48367c357bc716dee14e9d0fe
Gavin_Galusha <gavingalusha@gmail.com>
2025-06-16 10:40:07 +0300

    cleanup

--- src/Database/Chroma.py
+++ src/Database/Chroma.py
@@ -2,33 +2,34 @@ import chromadb
 import json
 
 chroma_client = chromadb.PersistentClient(path="database.chroma")
-collection = chroma_client.get_or_create_collection(name="my_collection")
+collection1 = chroma_client.get_or_create_collection(name="my_collection")
 
 documents = []
 ids = []
 metadatas = []
 
 # Open the JSONL-style file (one JSON object per line)
-with open("../api_descriptions.json", "r") as f:
+with open("API/sample_data/api_descriptions_deepseek_coder_6pt7b.json", "r") as f:
     for i, line in enumerate(f):
         api = json.loads(line.strip())  # Parse each line as a JSON object
 
         file = api["file"]
         endpoints = api.get("endpoints", "Empty")
-        print(endpoints)
         summary = endpoints.get("api_summary", "Empty")
         methods = str(endpoints.get("methods", "Empty"))
         paths = str(endpoints.get("paths", "Empty"))
 
 
-
+        
         documents.append(summary)
+        print()
         ids.append(f"id_{i}")
         metadatas.append({
             "file": file,
             "methods": methods,
             "paths": paths
         })
+        print(i)
 
 
 print("Document Length:", len(documents))
@@ -37,20 +38,22 @@ print("Document Length:", len(documents))
 
 
 #Uncomment when we want to update database
-''' 
-collection.upsert(
+collection1.upsert(
     documents=documents,
     ids=ids,
     metadatas=metadatas
 )
 
-'''
+
 
 print("Database Created")
 input = str(input("enter an api, and we will return suggested apis \n"))
 
+
+
+
 def find_similar_apis(text_input, top_k = 4):
-    results = collection.query(
+    results = collection1.query(
     query_texts=[input],
     n_results=4
     )
@@ -58,4 +61,4 @@ def find_similar_apis(text_input, top_k = 4):
     
 
 
-print(find_similar_apis(input))
+print(find_similar_apis())
